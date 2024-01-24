374bd2a9bcdb6756abdbb8385da105c51f5f1e2c
Esaú Bonilla <esafb007@gmail.com>
2024-01-24 10:09:15 -0600

    Envío 3

---
+++ Agro/Agro.web/Data/Entities/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Agro.web.Data.Entities
+{
+    public class City
+    {
+        //Creado por AEFB
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
